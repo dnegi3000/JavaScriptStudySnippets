8940aaad3764d52a1a9d009197b5d5a0030edffd
Deepak Negi <dnegi3000@gmail.com>
2017-01-02 01:30:38 +0530

    commit

--- promise.js
+++ promise.js
@@ -3,12 +3,22 @@ console.log("*******************************************");
 console.log("*****************Promises.js**************************");
 let somePromise  = new Promise(
     (resolve,reject) =>{
-        console.log(resolve,reject);    
+        let success = false;
+        if (success){
+        console.log('Successs**-------------->');
+        resolve('success');
+        } else {
+        console.log('error***-------------->');
+        reject('error');
+        console.log('after***-------------->');
+       
+        }
     }        
-    
 );
 
+//console.log('--->',somePromise);
+somePromise.then((message)=> console.log('Than resolve  +' + message)).catch(messager=> { console.log('this  one'+ messager);});
 
-console.log('--->',somePromise);
-console.log('**********',somePromise.then(()=> console.log('Than resolve')
-                                         ));
+console.log("====================");
+somePromise.catch((message)=> console.log('again than   ' + message))
+console.log("--------------" + somePromise.prototype);
