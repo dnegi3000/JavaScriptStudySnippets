39ccedcaf2427b3590c84766a2b5928b2bbddbf8
dnegi <a@email.com>
2017-11-13 01:20:26 +0530

    commit

--- bouncingball/main-breaking.js
+++ bouncingball/main-breaking.js
@@ -5,11 +5,33 @@ var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
 
+var ball ;
+var paddle ;
+
+var START_X=2;
+var START_Y=1;
+
+var rightPressed = false;
+var leftPressed = false;
+
+var STOP_GAME = false ;
+
+
 // function to generate random number
 
 function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
+}
+
+//Ball 
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
 }
 
 //paddle
@@ -23,18 +45,35 @@ function Paddle(x,y){
 
 }
 
-//Ball 
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
+
+//The wall 
+
+/*fucntion  Wall(x,y,rowNum,columnNum){
+
+    this.x =  x;
+    this.y=y;
+    this.rowNum=rowNUm ;
+    this.cloumnNUm= columnNUm;
+    this.brickArray;  
+
+
+
 }
+*/
 
-
+//Update method for the ball 
+// UPdate the position
 Ball.prototype.update = function() {
+
+  
+  if (paddle == undefined ) 
+  {
+    console.log("paddle is initialiazed ");
+    return;
+  }
+
+  
+
   if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
@@ -43,18 +82,31 @@ Ball.prototype.update = function() {
     this.velX = -(this.velX);
   }
 
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
+  if ( (this.x < paddle.x) || (this.x <= paddle.x+paddle.width) ){
+    
+      if ((this.y + this.size) >= (height- paddle.height)) {
+        this.velY = -(this.velY);
+      }
+
+      if ((this.y - this.size) <= paddle.height) {
+        this.velY = -(this.velY);
+      }
+    
+  }
+  //Condotion for game over
+  if ((this.y + this.size) >= (height)) {
+    STOP_GAME = true;
   }
 
   if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
+    STOP_GAME = true;
   }
 
   this.x += this.velX;
   this.y += this.velY;
 }
 
+//Prototype the draw method on ball 
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
@@ -62,12 +114,8 @@ Ball.prototype.draw = function() {
   ctx.fill();
 }
 
+//Draw method on the Paddle 
 Paddle.prototype.draw = function(){
-//    if(rightPressed)
-//     { 
-//      this.x
-//     }
-//
     ctx.beginPath();
     ctx.fillStyle= this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -96,19 +144,21 @@ Paddle.prototype.update = function(){
 }
 
 
-var balls = [];
-var paddle ;
-var START_X=1;
-var START_Y=1;
-var rightPressed = false;
-var leftPressed = false;
+
 
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
-  while (balls.length < 1) {
-    var ball = new Ball(
+  if (STOP_GAME)
+    {
+     document.getElementById('score').innerHTML ='Game Over'  ;
+
+      return ;
+    } 
+
+  if (ball == undefined) {
+       ball = new Ball(
       random(0,width),
       random(0,height),
       START_X,
@@ -116,13 +166,12 @@ function loop() {
       'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
       10
     );
-    balls.push(ball);
+   
   }
-
-  for (var i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-   }
+  if (ball != undefined){
+    ball.draw();
+    ball.update();
+  }
   
    if (paddle)
    {
@@ -133,8 +182,8 @@ function loop() {
         paddle = new Paddle(0,height-20);
     }
    paddle.draw();
-   
    requestAnimationFrame(loop);
+
 }
 
 function keyDownHandler(e) {
